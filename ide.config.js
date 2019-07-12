834887da0601ae9a57b279d4be20c16a1a263b2e
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-07-12 11:42:53 +0800

    add config

---
+++ ide.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "name": "[NAME]",
+    "className": "[CLASSNAME]",
+    "libName": "[LIBNAME]",
+    "proxyLibs": [],  // 要代理的联调的包, 支持字符串或者对象格式
+    "externals": [], // 想要 externals 的列表，完整对象格式
+}
