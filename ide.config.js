dd0ef58f2d5e14ad1b1fab1e0ab0bc1d238873cb
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-08-08 08:50:12 +0800

    升级依赖

--- ide.config.js
+++ ide.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "className": "[CLASSNAME]",
     "libName": "[LIBNAME]",
     "proxyLibs": [],  // 要代理的联调的包, 支持字符串或者对象格式
+    "proxyLabPathPrefix": '..', // 需要代理的联调的包路径前缀
     "externals": [], // 想要 externals 的列表，完整对象格式
 }
