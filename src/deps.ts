69987b3256bdf00adc41a3d4eb845c3dee0a13ff
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2020-04-02 17:58:25 +0800

    标准化动态加载

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,19 @@
+/* ----------------------------------------------------
+    支持动态加载资源的入口文件，请勿更改当前文件名
+----------------------------------------------------- */
+import { loadScriptsQueue, IScriptsLoadResult, loadScript } from 'ide-lib-utils';
+let lastLoadResult: IScriptsLoadResult = {};
+
 const UNPKG_DOMAIN = '//unpkg.com/ide-lib-engine/dist/index.umd.js';
 const getUnpkgPath = (name: string, version?: string, midPath?: string, fileName = 'index.umd.js') => {
     const middlePath = midPath || 'dist';
     return !!version ? `${UNPKG_DOMAIN}/${name}/${middlePath}/${fileName}` : `${UNPKG_DOMAIN}/${name}@${version}/${middlePath}/${fileName}`;
 };
 // export const BASEURL = '//dev.g.alicdn.com/lf/lf-canvas-assets/0.1.6/externaljs/';
+
+declare const __PUBLIC_PATH__: string;
+
+const URL_INSTANCE_TREE = __PUBLIC_PATH__ + `index.dynamic.umd.js`;
 
 export const dependScripts = [
     {
@@ -63,3 +73,26 @@ export const dependScripts = [
         deps: ['ideModelUtils', 'ideBaseComponent']
     }
 ]
+
+// 动态加载依赖资源
+export const loadDependencies = () => loadScriptsQueue(dependScripts, {
+    lastLoadResult
+});
+
+
+// 支持从 CDN 上动态加载组件
+export const load[CLASSNAME]FromCDN = () => {
+    if (window.lf[CLASSNAME]) {
+        const { loadDependencies } = window.lf[CLASSNAME];
+        return loadDependencies()
+            /* STEP 1: 加载主体 index.dynamic 文件 */
+            .then(() => loadScript(URL_INSTANCE_TREE))
+            .then(() => {
+                if (window.lf[CLASSNAME].load[CLASSNAME]) {
+                    return window.lf[CLASSNAME].load[CLASSNAME]()
+                } else {
+                    throw new Error('[[CLASSNAME]] dynamic load from cdn error');
+                }
+            });
+    }
+}
--- src/index.dynamic.tsx
+++ src/index.dynamic.tsx
@@ -1,56 +1,37 @@
 /* ----------------------------------------------------
     支持动态加载资源的入口文件，请勿更改当前文件名
 ----------------------------------------------------- */
-import {
-  loadScriptsQueue,
-  IScriptsLoadResult,
-  mitt
-} from 'ide-lib-utils';
+import { mitt } from 'ide-lib-utils';
 
-import { dependScripts } from './deps';
-
-// let mainModuleOrNull: IPlainObject = {};
-let lastLoadResult: IScriptsLoadResult = {};
+import { debugComp } from './lib/debug';
+import { loadDependencies } from './deps';
 
 // 动态引入类型，可以知道当前模块导出的变量（不包含 interface 等内容）
 export type T[CLASSNAME] = typeof import('.'); // This is the import type!
 
-// export interface  extends typeof import('./mainModule') {};
-export type ModuleLoadedHandler = (mainModule: T[CLASSNAME]) => void;
-
 const emitter = mitt();
 
+// 简单的封装，用于动态加载
 export const load[CLASSNAME] = () => {
-  const promise = new Promise((resolve, reject) => {
+  return import(/* webpackChunkName: "main" */ './index');
+};
+
+export const load[CLASSNAME]WithDeps = () => {
+  return new Promise((resolve, reject) => {
     emitter.on('loaded:[CLASSNAME]', (e: { mainModule: T[CLASSNAME] }) => {
       resolve(e && e.mainModule);
     });
-
     emitter.on('noload:[CLASSNAME]', (e: { error: any }) => {
       reject(e && e.error);
     });
+    loadDependencies()
+      .then(async result => {
+        debugComp('[load[CLASSNAME] with deps] load result: ', result);
+        const mainModule = await load[CLASSNAME]();
+        emitter.emit('loaded:[CLASSNAME]', { mainModule });
+      })
+      .catch(err => {
+        emitter.emit('noload:[CLASSNAME]', { error: err });
+      });
   });
-
-  console.log('Start loading [CLASSNAME] Module...');
-
-  loadScriptsQueue(dependScripts, {
-    lastLoadResult
-  })
-    .then(result => {
-      console.log('load result: ', result);
-      // 再动态加载组件
-      import(/* webpackChunkName: "main" */ './index').then(
-        mainModule => {
-          emitter.emit('loaded:[CLASSNAME]', { mainModule });
-        }
-      );
-    })
-    .catch(err => {
-      emitter.emit('noload:[CLASSNAME]', { error: err });
-    });
-
-  return promise;
 };
-
-
-
